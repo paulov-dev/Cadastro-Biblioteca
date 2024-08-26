2d99f756017dacf7bca30e2e47b7870736b05d52
paulov-dev <paulovmmf@gmail.com>
2024-08-26 18:09:51 -0300

    Adicionar arquivos de projeto.

---
+++ Biblioteca/FrmMenu.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Biblioteca
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            menuStrip1 = new MenuStrip();
+            cadastrosToolStripMenuItem = new ToolStripMenuItem();
+            MnuGenero = new ToolStripMenuItem();
+            MnuSair = new ToolStripMenuItem();
+            MnuSuspenso = new ContextMenuStrip(components);
+            MnSGenero = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            MnuSuspenso.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cadastrosToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1059, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            cadastrosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { MnuGenero, MnuSair });
+            cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            cadastrosToolStripMenuItem.Size = new Size(71, 20);
+            cadastrosToolStripMenuItem.Text = "&Cadastros";
+            // 
+            // MnuGenero
+            // 
+            MnuGenero.Name = "MnuGenero";
+            MnuGenero.Size = new Size(180, 22);
+            MnuGenero.Text = "&Gênero";
+            MnuGenero.Click += MnuGenero_Click;
+            // 
+            // MnuSair
+            // 
+            MnuSair.Name = "MnuSair";
+            MnuSair.Size = new Size(180, 22);
+            MnuSair.Text = "Sai&r";
+            MnuSair.Click += MnuSair_Click;
+            // 
+            // MnuSuspenso
+            // 
+            MnuSuspenso.Items.AddRange(new ToolStripItem[] { MnSGenero });
+            MnuSuspenso.Name = "MnuSuspenso";
+            MnuSuspenso.Size = new Size(181, 48);
+            // 
+            // MnSGenero
+            // 
+            MnSGenero.Name = "MnSGenero";
+            MnSGenero.Size = new Size(180, 22);
+            MnSGenero.Text = "&Gênero";
+            MnSGenero.Click += MnSGenero_Click;
+            // 
+            // FrmMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1059, 564);
+            ContextMenuStrip = MnuSuspenso;
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "FrmMenu";
+            Text = "Sistema de biblioteca";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            MnuSuspenso.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem cadastrosToolStripMenuItem;
+        private ToolStripMenuItem MnuGenero;
+        private ToolStripMenuItem MnuSair;
+        private ContextMenuStrip MnuSuspenso;
+        private ToolStripMenuItem MnSGenero;
+    }
+}
---
+++ Biblioteca/FrmMenu.cs
@@ -0,0 +1,28 @@
+namespace Biblioteca
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MnuSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void MnuGenero_Click(object sender, EventArgs e)
+        {
+            FrmGenero oFrm = new FrmGenero();
+            oFrm.MdiParent = this;
+            oFrm.Show();
+        }
+
+        private void MnSGenero_Click(object sender, EventArgs e)
+        {
+            MnuGenero_Click(sender, e);
+        }
+    }
+}
